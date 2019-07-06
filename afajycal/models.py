59bc9910d7b2858a5b570095f4dd6c9f6d20b029
Hiroki Takeda <takedahiroki@gmail.com>
2019-07-06 17:19:15 +0900

    add google calendar link

--- afajycal/models.py
+++ afajycal/models.py
@@ -1,3 +1,7 @@
+import urllib.parse
+from datetime import timedelta
+
+
 class MatchSchedule:
     """
     AFA junior youth soccer match schedule.
@@ -30,3 +34,31 @@ class MatchSchedule:
         self.home_team = kwargs['home_team']
         self.away_team = kwargs['away_team']
         self.studium = kwargs['studium']
+
+    def google_calendar_link(self):
+        """
+        output a url string for adding to google calendar.
+
+        Returns
+        -------
+        link : str
+            a url for adding to google calendar.
+
+        """
+        title = self.category + ' (' + self.home_team + ' vs ' \
+            + self.away_team + ')'
+        start_date = self.kickoff_time
+        if self.category == 'サテライト':
+            game_time = 60
+        else:
+            game_time = 90
+
+        end_date = self.kickoff_time + timedelta(minutes=game_time)
+        start = start_date.strftime('%Y-%m-%dT%H:%M:%S%z')
+        end = end_date.strftime('%Y-%m-%dT%H:%M:%S%z')
+        link = 'https://www.google.com/calendar/event?' \
+            + 'action=' + 'TEMPLATE' \
+            + '&text=' + urllib.parse.quote(title) \
+            + '&location=' + urllib.parse.quote(self.studium) \
+            + '&dates=' + start + '/' + end
+        return link
--- tests/test_models.py
+++ tests/test_models.py
@@ -17,6 +17,18 @@ class TestMatchSchedule(unittest.TestCase):
             'away_team': '中富良野',
             'studium': '花咲球技場'})
 
+    def test_google_calendar_link(self):
+        expect = 'https://www.google.com/calendar/event?' \
+                + 'action=TEMPLATE' \
+                + '&text=' \
+                + '%E3%82%B5%E3%83%86%E3%83%A9%E3%82%A4%E3%83%88%20%28%E5%85%AD%E5%90%88%20vs%20%E4%B8%AD%E5%AF%8C%E8%89%AF%E9%87%8E%29' \
+                + '&location=' \
+                + '%E8%8A%B1%E5%92%B2%E7%90%83%E6%8A%80%E5%A0%B4' \
+                + '&dates=' + '2019-06-02T14:00:00+0900' \
+                + '/' + '2019-06-02T15:00:00+0900'
+        result = self.schedule.google_calendar_link()
+        self.assertEqual(result, expect)
+
 
 if __name__ == '__main__':
     unittest.main()
