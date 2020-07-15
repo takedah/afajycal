2f0d69e4166c5b92f29b97ea68a8080bc113d267
Hiroki Takeda <takedahiroki@gmail.com>
2020-07-15 22:40:58 +0900

    fixed.

--- import_schedules.py
+++ import_schedules.py
@@ -4,9 +4,6 @@ from afajycal.models import ScheduleFactory, ScheduleService
 from afajycal.scraper import (
     DownloadedHTML,
     ScrapedHTMLData,
-    DownloadedExcel,
-    ScrapedExcelData,
-    HTMLDownloadError,
 )
 
 
@@ -15,37 +12,21 @@ def import_schedules():
     """
 
     # Webサイトからデータを抽出する処理
-    try:
-        downloaded_html = DownloadedHTML(
-            "http://afa11.com/asahijy/reiwa2/nittei2020.html"
-        )
-        scraped_data = ScrapedHTMLData(downloaded_html)
-        # downloaded_excel = DownloadedExcel('http://afa11.com/asahijy/reiwa2/nittei2020.xlsx')
-        # scraped_data = ScrapedExcelData(downloaded_excel)
-    except HTMLDownloadError as e:
-        print("ダウンロードに失敗しました。")
-        print(e.args[0])
-
+    downloaded_html = DownloadedHTML("http://afa11.com/asahijy/reiwa2/nittei2020.html")
+    scraped_data = ScrapedHTMLData(downloaded_html)
     schedule_factory = ScheduleFactory()
     for d in scraped_data.schedule_data:
         schedule_factory.create(d)
 
     # 抽出データをデータベースへ格納する処理
+    db = DB(Config.DATABASE)
     try:
-        db = DB(Config.DATABASE)
-    except DatabaseError as e:
-        print("データベースに接続できませんでした。")
-        print(e.args[0])
-
-    schedule_service = ScheduleService(db)
-    schedule_service.truncate()
-    try:
+        schedule_service = ScheduleService(db)
         for schedule in schedule_factory.items:
             schedule_service.create(schedule)
         db.commit()
-    except DataError as e:
+    except (DatabaseError, DataError) as e:
         db.rollback()
-        print("データ保存に失敗しました。")
         print(e.args[0])
     finally:
         db.close()
